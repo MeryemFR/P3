be24c166aae66488d43f0e57002d828cb8deb144
meryem.frouni <meryemfrhdg@gmail.com>
2024-07-18 14:33:23 +0200

    ajouter le file login pour la page login

---
+++ FrontEnd/script/script.js
@@ -0,0 +1,22 @@
+//Récupération les données depuis l'API//
+
+//*** Récupération les données Works 
+let listWorks = []                                        // variable initialisée à un tab vide pour stocker les données                              
+async function getWorks() {                                 
+    await fetch('http://localhost:5678/api/works')          // appel de l'API
+          .then((response) => response.json())              // récup des données
+          .then((data) => {                                 // remplir le tableau 
+            listWorks=data                          
+          } )
+}
+
+
+//*** Récupération les données Catégories 
+let categories = []
+async function getCategories() {
+    await fetch('http://localhost:5678/api/categories')     
+          .then((response) => response.json())              
+          .then((data) => {                                 
+            categories=data                    
+          } )
+}
