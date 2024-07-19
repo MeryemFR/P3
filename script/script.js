cfb288811e25ae390bae36f66d540ad4fc57233c
meryem.frouni <meryemfrhdg@gmail.com>
2024-07-19 11:30:25 +0200

    réorganisation des dossiers

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
 //Récupération les données depuis l'API//
 
 //*** Récupération les données Works 
-let listWorks = []                                        // variable initialisée à un tab vide pour stocker les données                              
-async function getWorks() {                                 
+ let listWorks = []                                        // variable initialisée à un tab vide pour stocker les données                              
+async function getWorks() {                                  // Fonction asynchrone pour obtenir les données
     await fetch('http://localhost:5678/api/works')          // appel de l'API
           .then((response) => response.json())              // récup des données
           .then((data) => {                                 // remplir le tableau 
