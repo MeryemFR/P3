23d0181154bae845472f3a199460b926611c4c3f
meryem.frouni <meryemfrhdg@gmail.com>
2024-07-30 16:50:54 +0200

    Récupération API de login et sauvegarde du Token et puis rediriger vers Index

--- script/login.js
+++ script/login.js
@@ -36,16 +36,39 @@ function initializeValidation() {
 function login() {
 
     //récup formulaire login
-    const form = document.getElementsByClassName('login')
-
-    
+    const form = document.querySelector('.login')   
     //Event bouton se connecter
-    form.addEventListener('submit', (event) => {
+    form.addEventListener('submit', (event) => {      
         event.preventDefault()
         //récup valeur saisies dans email et password
-        const email = document.getElementById("email").value
-        const password = document.getElementById("password").value
+        const email = document.getElementById("email-bar").value
+        const password = document.getElementById("password-bar").value
 
         //appel de l'API de login
         fetch('http://localhost:5678/api/users/login', {
-            
+            method: 'POST',
+            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json'},
+            body: JSON.stringify({email,password})})
+        .then(response => response.json())
+        .then(data => {
+            if (data.token) {
+                //sauvegarde du token dans le localStorage
+                localStorage.setItem('token', data.token)           
+                window.location.href = "index.html"       
+            } else {
+                    // Vérifier si le msg d'erreur existe déjà
+                    const existingErrorMsg = form.querySelector('.error-msg')
+                    if (!existingErrorMsg) {
+                        // Créer msg d'erreur si aucun n'existe
+                        const errorMsg = document.createElement('div')
+                        errorMsg.classList.add('error-msg')
+                        errorMsg.textContent = "Please check your email and password"
+                        form.appendChild(errorMsg)
+                
+                        //css pour le message d'erreur
+                        errorMsg.style.cssText = 'color: red; font-size: 16px; align-self: center; margin-bottom: 10px;'
+                    }
+                }
+        })
+    })
+}
